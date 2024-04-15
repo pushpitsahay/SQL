485cb17d451367a4b2d86e9873ec9699c6c0f3c1
Pushpit Sahay <87263421+pushpitsahay@users.noreply.github.com>
2024-04-16 00:15:37 +0530

    Create LeetCode1667_FixNamesInATable

---
+++ SQL 50/LeetCode1667_FixNamesInATable.sql
@@ -0,0 +1,4 @@
+-- https://leetcode.com/problems/fix-names-in-a-table/?envType=study-plan-v2&envId=top-sql-50
+SELECT user_id, CONCAT(UPPER(LEFT(name, 1)), LOWER(RIGHT(name, LENGTH(name)-1))) AS name
+FROM Users
+ORDER BY user_id;
