30e3871d296a6e830c3a51d4413cfd7445bf9944
Pushpit Sahay <87263421+pushpitsahay@users.noreply.github.com>
2024-05-04 15:54:04 +0530

    Update and rename LeetCode1729_FindFollowersCount to LeetCode1729_FindFollowersCount.sql

--- SQL 50/LeetCode1729_FindFollowersCount.sql
+++ SQL 50/LeetCode1729_FindFollowersCount.sql
@@ -1,4 +1,4 @@
-https://leetcode.com/problems/find-followers-count/?envType=study-plan-v2&envId=top-sql-50
+-- https://leetcode.com/problems/find-followers-count/?envType=study-plan-v2&envId=top-sql-50
 SELECT user_id, COUNT(DISTINCT follower_id) AS followers_count
 FROM Followers
 GROUP BY user_id
